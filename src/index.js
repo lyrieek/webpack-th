e2db7ad924a0aa6f61599ee6abeea15f93ac3fa1
lyrieek <tianhao7@foxmail.com>
2018-12-23 23:50:29 +0800

    re build

--- src/index.js
+++ src/index.js
@@ -2,10 +2,51 @@ const merge = require('webpack-merge')
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 export default class WebpackTH {
-	constructor() {
+
+	constructor(initConfig) {
 		this.version = '1.0.1';
+		this.config = initConfig || {};
 	}
+
+	add(addConfig) {
+		this.config = merge(this.config, addConfig);
+	}
+
+	setPath(intput, output) {
+		this.intput = intput;
+		this.output = output;
+	}
+
+	addHTML(array) {
+		if (!(array instanceof Array)) {
+			array = [array];
+		}
+		array.map((fileName) =>
+			config.plugins.push(
+				new HtmlWebpackPlugin({
+					filename: this.intput + fileName + '.html',
+					template: this.output + fileName + '.html',
+					inject: true,
+					minify: {
+						removeComments: true,
+						collapseWhitespace: true,
+						removeAttributeQuotes: true,
+						minifyCSS: true,
+						minifyJS: true
+					},
+					chunks: [],
+					chunksSortMode: 'dependency'
+				})
+			))
+	}
+
+	config(){
+		return this.config;
+	}
+
 }
